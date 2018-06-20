fd681c49f92343e1a77a0572a65795bfc80908e2
puppitty <33668867+puppitty@users.noreply.github.com>
2018-06-20 13:26:46 -0400

    Working game-some styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Nav = props => (
-  <div className="container-fluid text-center">
-    <strong><p>Click an image of a bird to score</p>
-      <p>Be sure not to click the same image twice!</p>
-      <p>Score: {props.currentScore}</p>
-      <p>High Score: {props.highScore}</p></strong>
+  <div className="container-fluid mt-0">
 
+    <nav className="navbar navbar-dark bg-primary  justify-content-between">
+      <span className="navbar-brand mb-0"><h1><strong>Bird Clicky Game</strong></h1></span>
+      <span className="rightnav"><h3><strong>Score: {props.currentScore} | High Score: {props.highScore}</strong></h3></span>
+    </nav>
 
   </div>
+
 );
 export default Nav;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Title = props => (
+  <div className="titlearea">
 
+
+    <p>Click on an image of a bird to score, but be careful not to click on the same image twice!</p>
+
+  </div>
+);
 export default Title;
