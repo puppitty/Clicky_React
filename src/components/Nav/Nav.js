c07e6eb83e873f576929dda1b01792541607f89c
puppitty <33668867+puppitty@users.noreply.github.com>
2018-06-20 13:57:45 -0400

    fixed styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = props => (
-  <div className="container-fluid mt-0">
+  <div className="container-fluid">
 
-    <nav className="navbar navbar-dark bg-primary  justify-content-between">
+    <nav className="navbar fixed-top navbar-light bg-light  justify-content-between">
       <span className="navbar-brand mb-0"><h1><strong>Bird Clicky Game</strong></h1></span>
       <span className="rightnav"><h3><strong>Score: {props.currentScore} | High Score: {props.highScore}</strong></h3></span>
     </nav>
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,9 +3,11 @@ import "./Title.css";
 
 const Title = props => (
   <div className="titlearea">
+    <div className="container">
 
-
-    <p>Click on an image of a bird to score, but be careful not to click on the same image twice!</p>
+      <p>Click on an image of a bird to score, </p>
+      <p>but be careful not to click on the same image twice!</p>
+    </div>
 
   </div>
 );
